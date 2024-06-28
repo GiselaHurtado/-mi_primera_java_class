7a4eaeb942d469219bfee0744befd0cf1fdb43a1
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-06-28 21:54:51 +0200

    first exercise about person done

--- src/main/java/dev/gisela/mi_primera_java_class/models/Person.java
+++ src/main/java/dev/gisela/mi_primera_java_class/models/Person.java
@@ -3,12 +3,32 @@ package dev.gisela.mi_primera_java_class.models;
 public class Person {
     
     private String name;
+    private String lastName;
+    private String dni;
+    private int bornDate;
 
-    public Person(String name) {
+    public Person(String name, String lastName, String dni, int bornDate) {
         this.name = name;
+        this.lastName = lastName;
+        this.dni = dni;
+        this.bornDate = bornDate;
     }
+
 
     public String getName(){
         return name;
     }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+   public String getDni(){
+   return dni;
+   }
+
+   public int getBornDate(){
+    return bornDate;
+   }
+
 }
--- src/test/java/dev/gisela/mi_primera_java_class/models/PersonTest.java
+++ src/test/java/dev/gisela/mi_primera_java_class/models/PersonTest.java
@@ -9,16 +9,40 @@ public class PersonTest {
     @Test
     void testPersonHasAttributes() {
 
-        Person carla = new Person("Carla");
-        assertEquals(1, carla.getClass().getDeclaredFields().length);
+        Person carla = new Person("Carla","Par","12345678N",22081991);
+        assertEquals(4, carla.getClass().getDeclaredFields().length);
 
     }
 
     @Test
     void testGetName() {
 
-        Person Carla = new Person("Carla");
+        Person Carla = new Person("Carla","Par","12345678N", 22081991);
         assertEquals("Carla", Carla.getName());
+
+    }
+
+    @Test
+    void testGetLastName() {
+
+        Person Carla = new Person("Carla","Par", "12345678N", 22081991);
+        assertEquals("Par", Carla.getLastName());
+
+    }
+
+    @Test
+    void testGetDni() {
+
+        Person Carla = new Person("Carla","Par", "12345678N", 22081991);
+        assertEquals("12345678N", Carla.getDni());
+
+    }
+
+    @Test
+    void testGetBornDate() {
+
+        Person Carla = new Person("Carla","Par", "12345678N", 22081991);
+        assertEquals(22081991, Carla.getBornDate());
 
     }
 }
